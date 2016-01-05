cf0d0753678ad46c475818785063244aae5474e1
kioko <kiokotomas@gmail.com>
2016-01-05 17:53:21 +0300

    Created Meme class object

---
+++ Meme Creator/Meme.swift
@@ -0,0 +1,15 @@
+//
+//  Meme.swift
+//  Meme Creator
+//
+//  Created by Kioko on 05/01/2016.
+//  Copyright © 2016 Thomas Kioko. All rights reserved.
+//
+
+import UIKit
+
+class Meme {
+    var topText: String!
+    var bottomText: String!
+    var originalImage: UIImage!
+}
