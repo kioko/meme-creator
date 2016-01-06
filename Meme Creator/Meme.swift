38ff82a492e4ae13e80f67c43ccb6b1d3aac5f72
kioko <kiokotomas@gmail.com>
2016-01-06 13:59:45 +0300

    Added Initialise function

--- Meme Creator/Meme.swift
+++ Meme Creator/Meme.swift
@@ -12,4 +12,13 @@ class Meme {
     var topText: String!
     var bottomText: String!
     var originalImage: UIImage!
+    var memedImage: UIImage!
+    
+    init(topText: String, bottomText: String, originalImage: UIImage,
+        memedImage:UIImage){
+            self.topText = topText
+            self.bottomText = bottomText
+            self.originalImage = originalImage
+            self.memedImage = memedImage
+    }
 }
