723fc2594d5e753432c76ab4325c6879bdab9d61
kioko <kiokotomas@gmail.com>
2016-01-06 23:57:28 +0300

    Created table cell class

---
+++ Meme Creator/MemeTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MemeTableViewCell.swift
+//  Meme Creator
+//
+//  Created by Kioko on 06/01/2016.
+//  Copyright © 2016 Thomas Kioko. All rights reserved.
+//
+
+import UIKit
+
+class MemeTableViewCell: UITableViewCell {
+    @IBOutlet weak var memeImageView: NSLayoutConstraint!
+    @IBOutlet weak var memeLabel: UILabel!
+}
